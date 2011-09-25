2166eff786bd841c34c0e72dab1acddd59a6e879
Fernando Michelotti <artista.frustrado@gmail.com>
2011-09-24 23:40:11 -0300

    added blender compiling script

---
+++ compile/Blender-opt-SVN.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get install subversion build-essential gettext libxi-dev libsndfile1-dev libpng12-dev libfftw3-dev libopenexr-dev libopenjpeg-dev libopenal-dev libalut-dev libvorbis-dev libglu1-mesa-dev libsdl1.2-dev libfreetype6-dev libtiff4-dev libsamplerate0-dev libavdevice-dev libavformat-dev libavutil-dev libavcodec-dev libjack-jackd2-dev libswscale-dev libx264-dev libmp3lame-dev python3.2-dev libspnav-dev cmake cmake-curses-gui
+
+mkdir -p /usr/src/frustrado/blender
+cd /usr/src/frustrado/blender 
+svn co https://svn.blender.org/svnroot/bf-blender/trunk/blender
+
+mkdir build-blender
+cd build-blender
+
+cmake ../blender -DCMAKE_INSTALL_PREFIX=/opt/blender -DWITH_INSTALL_PORTABLE=OFF && make && make install 
+
+#              -DCMAKE_INSTALL_PREFIX=/opt/blender \
+#              -DWITH_INSTALL_PORTABLE=OFF \
+#              -DWITH_BUILDINFO=OFF \
+#              -DWITH_GAMEENGINE=OFF
