4f38e1b1b6639b8d5d1b957c486eb2b45bb409ed
Fernando Michelotti <fernando.michelotti@cnt.com.br>
2011-11-28 10:36:18 -0200

    Added Drizzle and MyPaint

--- compile/Blender-SVN.sh
+++ compile/Blender-SVN.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install subversion build-essential gettext  libxi-dev libsndfile1-dev libpng12-dev libfftw3-dev libopenexr-dev libopenjpeg-dev  libopenal-dev libalut-dev libvorbis-dev libglu1-mesa-dev libsdl1.2-dev libfreetype6-dev libtiff4-dev libsamplerate0-dev libavdevice-dev libavformat-dev libavutil-dev libavcodec-dev libjack-dev libswscale-dev libx264-dev libmp3lame-dev python3.2-dev  libspnav-dev
-
+apt-get install subversion build-essential gettext  libxi-dev libsndfile1-dev libpng12-dev libfftw3-dev libopenexr-dev libopenjpeg-dev  libopenal-dev libalut-dev libvorbis-dev libglu1-mesa-dev libsdl1.2-dev libfreetype6-dev libtiff4-dev libsamplerate0-dev libavdevice-dev libavformat-dev libavutil-dev libavcodec-dev  libjack-jackd2-0 libjack-jackd2-dev libswscale-dev libx264-dev libmp3lame-dev python3.2-dev  libspnav-dev cmake cmake-curses-gui
 
 mkdir -p /usr/src/frustrado/blender-svn
 cd /usr/src/frustrado/blender-svn
-
 svn co https://svn.blender.org/svnroot/bf-blender/trunk/blender
 
-sudo apt-get install cmake cmake-curses-gui
-
-#cd /usr/src/frustrado/blender-svn/blender
-
+cd /usr/src/frustrado/blender-svn/blender
 mkdir /usr/src/frustrado/blender-svn/build
 cd /usr/src/frustrado/blender-svn/build
 cmake ../blender -DCMAKE_INSTALL_PREFIX=/opt/blender 
--- compile/MyPaint-GIT.sh
+++ compile/MyPaint-GIT.sh
@@ -4,7 +4,15 @@
 apt-get build-dep mypaint
 
 cd /usr/src/frustrado
-#git clone git://gitorious.org/mypaint/mypaint.git
+
+if [ -d /usr/src/frustrado/mypaint ]
+then
+	cd mypaint
+	git pull
+else
+	git clone git://gitorious.org/mypaint/mypaint.git
 cd mypaint
+fi
+
 scons prefix=/opt/git/mypaint install
 
