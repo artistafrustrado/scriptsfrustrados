f218edc654e6739473fee2fc1bbe1451c776bcb8
Fernando Michelotti <artista@frustrado.com.br>
2011-11-28 21:20:37 -0200

    fixed stuff

--- compile/Inkscape-Bazaar.sh
+++ compile/Inkscape-Bazaar.sh
@@ -25,7 +25,7 @@ apt-get build-dep inkscape
 apt-get install libgc-dev libgnomevfs2-dev libgtkmm-2.4-dev libxslt1-dev libsigc++-2.0-dev libgsl0-dev libboost-dev libpopt-dev libpoppler-glib-dev libwpg-dev imagemagick libmagick++-dev create-resources 
 
 ./autogen.sh
-./configure --prefix=/opt/inkscape-bzr --enable-lcms --enable-inkboard --with-gnome-vfs  --with-perl  --with-python && make && make install
+./configure --prefix=/opt/bzr/inkscape --enable-lcms --enable-inkboard --with-gnome-vfs  --with-perl  --with-python && make && make install
 
 cat > /usr/share/applications/inkscape-bzr.desktop << EOF
 [Desktop Entry]
--- compile/Scribus-SVN.sh
+++ compile/Scribus-SVN.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
-apt-get install gcc g++ cmake subversion  ghostscript qt-devel pkg-config lcms-devel libjpeg-devel libtiff-devel libart_lgpl-devel cups-devel libxml2 and libxml2-devel – you should already have libxml2 python and python-devel fontconfig-devel openssl-devel freetype and freetype-devel python-imaging-devel tk tkinter cairo acairo-devel qt4 qt4-devel 
+apt-get install gcc g++ cmake subversion  ghostscript qt-devel pkg-config lcms-devel libjpeg-devel libtiff-devel libart_lgpl-devel cups-devel libxml2 and libxml2-devel – you should already have libxml2 python and python-devel fontconfig-devel openssl-devel freetype and freetype-devel python-imaging-devel tk tkinter cairo cairo-devel qt4 qt4-devel 
 
-apt-get build-dep scribus
+#apt-get build-dep scribus
+apt-get build-dep scribus-ng
 
 mkdir /usr/src/frustrado/scribus -p
 mkdir /opt/svn/scribus -p
@@ -11,14 +12,16 @@ cd /usr/src/frustrado/scribus
 
 DIR="Scribus"
 
-if [ -d $DIR ]; then
+if [ -d "$DIR" ]; then
 	echo "Directory exists - UPDATING"
 	cd $DIR
 	svn update
 else 
 	echo "Directory does not exists - CHECKING OUT"
 #	svn co svn://scribus.info/Scribus/trunk/Scribus
-	svn co https://scribus.svn.sourceforge.net/svnroot/scribus $DIR 
+#	svn co https://scribus.svn.sourceforge.net/svnroot/scribus $DIR 
+#	svn co svn://scribus.net/branches/Version150/Scribus $DIR 
+	svn co svn://scribus.net/trunk/Scribus $DIR 
 	cd $DIR
 fi
 echo "COMPILING"
