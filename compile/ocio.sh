1aceb497a3ee1acdab056c0a87cc9cbf336036c8
Fernando Michelotti <artista@frustrado.com.br>
2013-01-28 02:11:59 -0200

    do not remember

--- compile/ocio.sh
+++ compile/ocio.sh
@@ -9,6 +9,7 @@ rm -fr /tmp/ociobuild
 mkdir /tmp/ociobuild
 cd /tmp/ociobuild
 
-cmake -D CMAKE_INSTALL_PREFIX=/opt/git/libs /usr/src/frustrado/calligra/OpenColorIO && make && make install
+#cmake -D CMAKE_INSTALL_PREFIX=/opt/git/libs /usr/src/frustrado/calligra/OpenColorIO && make && make install
+cmake -D CMAKE_INSTALL_PREFIX=/usr/local /usr/src/frustrado/calligra/OpenColorIO && make && make install
 
 cp /opt/git/libs/lib/pkgconfig/OpenColorIO.pc /usr/lib/pkgconfig/
