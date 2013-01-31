ef7867f1f81ccc649f4a6a51b16ec968ae00ff8b
Fernando Michelotti <artista@frustrado.com.br>
2013-01-31 10:25:37 -0200

    made key be downloaded to /tmp/ not apeending to sources file

--- compile/own-cloud.sh
+++ compile/own-cloud.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget http://download.opensuse.org/repositories/isv:ownCloud:devel/Debian_6.0/Release.key
-apt-key add - < Release.key 
+wget -O /tmp/Release.key http://download.opensuse.org/repositories/isv:ownCloud:devel/Debian_6.0/Release.key
+apt-key add - < /tmp/Release.key 
 
-echo 'deb http://download.opensuse.org/repositories/isv:ownCloud:devel/Debian_6.0/ /' >> /etc/apt/sources.list.d/owncloud-client.list 
+echo 'deb http://download.opensuse.org/repositories/isv:ownCloud:devel/Debian_6.0/ /' > /etc/apt/sources.list.d/owncloud-client.list 
 apt-get update
 apt-get install owncloud-client
